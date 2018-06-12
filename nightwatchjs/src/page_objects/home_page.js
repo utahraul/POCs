5cdb6eb38da561e9595565a638c61c947bd583a3
Raúl Hernández <rhernandez@idealista.com>
2018-06-12 17:44:29 +0200

    First test created

---
+++ nightwatchjs/src/page_objects/home_page.js
@@ -0,0 +1,9 @@
+module.exports = {
+  url: function() {
+    return this.api.launchUrl;
+  },
+  elements: {
+    here: ".rt-logo",
+    requestADemo: ".hero .btn"
+  }
+};
--- nightwatchjs/src/tests/home.js
+++ nightwatchjs/src/tests/home.js
@@ -1,13 +1,26 @@
 module.exports = {
-    'Demo test Google' : function (browser) {
-      browser
-        .url()
-        .waitForElementVisible('body', 1000)
-        .setValue('input[type=text]', 'nightwatch')
-        .waitForElementVisible('button[name=btnG]', 1000)
-        .click('button[name=btnG]')
-        .pause(1000)
-        .assert.containsText('#main', 'Night Watch')
-        .end();
-    }
-  };
+  "Demo test Google": function(browser) {
+    var homePage = browser.page.home_page();
+    var requestDemoPage = browser.page.request_demo_page();
+
+    homePage
+      .navigate()
+      .waitForElementVisible("@here", 5000)
+      .click("@requestADemo");
+
+    browser.windowHandles(function(result) {
+      browser.switchWindow(result.value[1]);
+    });
+
+    requestDemoPage
+      .waitForElementVisible("@here", 5000)
+      .assert.title("Returnly Demo | Turn Returns Into Repurchases")
+      .fillRequestDemoForm()
+      .sendRequestDemoForm();
+
+    browser
+      .waitForElementVisible('H1', 5000)
+      .assert.containsText('H1', 'Thank you!')
+      .end();
+  }
+};
